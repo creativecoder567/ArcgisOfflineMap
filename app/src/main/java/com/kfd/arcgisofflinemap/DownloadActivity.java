1dd2afb492800d0d891a50980bb1964a5bcf82f1
creativecoder567 <sarathcreativethinker@gmail.com>
2019-12-16 17:41:31 +0530

    Downloading Village is done using Download manager.

---
+++ app/src/main/java/com/kfd/arcgisofflinemap/DownloadActivity.java
@@ -0,0 +1,91 @@
+package com.kfd.arcgisofflinemap;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import java.net.URI;
+
+public class DownloadActivity extends AppCompatActivity {
+
+    EditText etUrl;
+    Button btnDownload;
+    private static final int PERMISSION_STORAGE=1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_download);
+
+        etUrl = findViewById(R.id.etUrl);
+        btnDownload = findViewById(R.id.btnDownload);
+
+        btnDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+
+                    if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_DENIED){
+
+                        String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        requestPermissions(permissions,PERMISSION_STORAGE);
+                    }else {
+                       startDownloading();
+                    }
+                }else {
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case PERMISSION_STORAGE:{
+                if (grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                  startDownloading();
+                }else {
+                    Toast.makeText(this, "Permission denied..", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        }
+    }
+
+    private void startDownloading() {
+        String url = etUrl.getText().toString();
+
+        if (!TextUtils.isEmpty(url)){
+            DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+
+            request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI|DownloadManager.Request.NETWORK_MOBILE);
+            request.setTitle("Download");
+            request.setDescription("Downloading File...");
+
+            request.allowScanningByMediaScanner();
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,System.currentTimeMillis()+".tpk");
+
+            DownloadManager manager  = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+            manager.enqueue(request);
+        }
+    }
+}
--- app/src/main/java/com/kfd/arcgisofflinemap/MainActivity.java
+++ app/src/main/java/com/kfd/arcgisofflinemap/MainActivity.java
@@ -51,14 +51,21 @@ public class MainActivity extends AppCompatActivity {
         String base = storageDir + File.separator + filename;
 
         ArcGISTiledLayer tiledLayerBaseMap = new ArcGISTiledLayer(base);
+        ArcGISTiledLayer tiledLayerBaseMap1 = new ArcGISTiledLayer(getResources().getString(R.string.world_topo_service));
+
+
 // set tiled layer as basemap
-        Basemap basemap = new Basemap(tiledLayerBaseMap);
+        Basemap basemap = new Basemap(tiledLayerBaseMap1);
+//        Basemap basemap1 = new Basemap(tiledLayerBaseMap1);
+//        basemap.getBaseLayers().add(tiledLayerBaseMap);
 // create a map with the basemap
         ArcGISMap map = new ArcGISMap(basemap);
+//        map.getBasemap().getBaseLayers().add(tiledLayerBaseMap);
+
 // set the map to be displayed in this view
         mMapView.setMap(map);
 
-
+//            setupMap();
         // get the MapView's LocationDisplay
          mLocationDisplay = mMapView.getLocationDisplay();
 
@@ -99,68 +106,22 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        mLocationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.COMPASS_NAVIGATION);
+        mLocationDisplay.startAsync();
 
 
-        // Populate the list for the Location display options for the spinner's Adapter
-        ArrayList<ItemData> list = new ArrayList<>();
-        list.add(new ItemData("Stop", R.drawable.locationdisplaydisabled));
-        list.add(new ItemData("On", R.drawable.locationdisplayon));
-        list.add(new ItemData("Re-Center", R.drawable.locationdisplayrecenter));
-        list.add(new ItemData("Navigation", R.drawable.locationdisplaynavigation));
-        list.add(new ItemData("Compass", R.drawable.locationdisplayheading));
-
-        SpinnerAdapter adapter = new SpinnerAdapter(this, R.layout.spinner_layout, R.id.txt, list);
-        mSpinner.setAdapter(adapter);
-        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-                switch (position) {
-                    case 0:
-                        // Stop Location Display
-                        if (mLocationDisplay.isStarted())
-                            mLocationDisplay.stop();
-                        break;
-                    case 1:
-                        // Start Location Display
-                        if (!mLocationDisplay.isStarted())
-                            mLocationDisplay.startAsync();
-                        break;
-                    case 2:
-                        // Re-Center MapView on Location
-                        // AutoPanMode - Default: In this mode, the MapView attempts to keep the location symbol on-screen by
-                        // re-centering the location symbol when the symbol moves outside a "wander extent". The location symbol
-                        // may move freely within the wander extent, but as soon as the symbol exits the wander extent, the MapView
-                        // re-centers the map on the symbol.
-                        mLocationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.RECENTER);
-                        if (!mLocationDisplay.isStarted())
-                            mLocationDisplay.startAsync();
-                        break;
-                    case 3:
-                        // Start Navigation Mode
-                        // This mode is best suited for in-vehicle navigation.
-                        mLocationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.NAVIGATION);
-                        if (!mLocationDisplay.isStarted())
-                            mLocationDisplay.startAsync();
-                        break;
-                    case 4:
-                        // Start Compass Mode
-                        // This mode is better suited for waypoint navigation when the user is walking.
-                        mLocationDisplay.setAutoPanMode(LocationDisplay.AutoPanMode.COMPASS_NAVIGATION);
-                        if (!mLocationDisplay.isStarted())
-                            mLocationDisplay.startAsync();
-                        break;
-                }
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) { }
-
-        });
     }
 
-
+    private void setupMap() {
+        if (mMapView != null) {
+            Basemap.Type basemapType = Basemap.Type.STREETS_VECTOR;
+            double latitude = 34.09042;
+            double longitude = -118.71511;
+            int levelOfDetail = 11;
+            ArcGISMap map = new ArcGISMap(basemapType, latitude, longitude, levelOfDetail);
+            mMapView.setMap(map);
+        }
+    }
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         // If request is cancelled, the result arrays are empty.
